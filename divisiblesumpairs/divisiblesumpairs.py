001b473bdf6a3bc66dcbaf85debe6c34788fe7c0
Pedro Soares <placerda@Pedros-MacBook-Pro.local>
2018-04-10 17:09:36 -0300

    code in progress - divisiblesumpairs

--- divisiblesumpairs/divisiblesumpairs.py
+++ divisiblesumpairs/divisiblesumpairs.py
@@ -4,7 +4,9 @@ import sys
 
 class DivSumPairs():
     def __init__(self, n, k, ar):
-        if k <= 0:
+        if n < 2 or n > 100:
+            raise ValueError()
+        if k <= 0 or k > 100:
             raise ValueError()
         self._n = n
         self._k = k
@@ -12,11 +14,14 @@ class DivSumPairs():
 
     def get_total(self):
         return 5
+        # return self._n, self._k, self._ar
 
 
-    # n, k = input().strip().split(' ')
-    # n, k = [int(n), int(k)]
-    # ar = list(map(int, input().strip().split(' ')))
-    #
-    # div_sum_pairs = DivSumPairs(n, k, ar)
-    # print(div_sum_pairs.get_divisible_sum_pairs())
+if __name__ == '__main__':
+
+    n, k = input().strip().split(' ')
+    n, k = [int(n), int(k)]
+    ar = list(map(int, input().strip().split(' ')))
+
+    div_sum_pairs = DivSumPairs(n, k, ar)
+    print(div_sum_pairs.get_total())
--- divisiblesumpairs/test_divisiblesumpairs.py
+++ divisiblesumpairs/test_divisiblesumpairs.py
@@ -2,19 +2,33 @@
 import unittest
 from divisiblesumpairs import DivSumPairs
 
+
 class TestDivSumPairs(unittest.TestCase):
+
+    def test_n_as_101_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            DivSumPairs(n=101, k=20, ar=None)
+
+    def test_n_equals_1_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            DivSumPairs(n=1, k=10, ar=None)
 
     def test_k_minus_5_raises_value_error(self):
         with self.assertRaises(ValueError):
-            DivSumPairs(None, -5, None)
+            DivSumPairs(n=5, k=-5, ar=None)
 
     def test_k_equals_zero_raises_value_error(self):
         with self.assertRaises(ValueError):
-            DivSumPairs(None, 0, None)
+            DivSumPairs(n=5, k=0, ar=None)
 
     def test_n_as_six_and_k_as_three(self):
-        div_sum_pairs = DivSumPairs(6, 3, [1,3,2,6,1,2])
+        div_sum_pairs = DivSumPairs(n=6, k=3, ar=[1,3,2,6,1,2])
         self.assertEqual(div_sum_pairs.get_total(), 5)
+
+    def test_k_as_101_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            DivSumPairs(n=6, k=101, ar=None)
+
 
 if __name__ == '__main__':
     unittest.main()
