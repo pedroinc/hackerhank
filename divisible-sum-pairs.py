5046560805b5e7120bd9bc5b0e89c81ced5a7a9c
Pedro Soares <pedroinc@gmail.com>
2018-04-10 01:12:51 -0300

    get_total() must be implemented

--- divisible-sum-pairs.py
+++ divisible-sum-pairs.py
@@ -7,24 +7,34 @@ class TestDivSumPairs(unittest.TestCase):
 
     def test_k_minus_5_raises_value_error(self):
         with self.assertRaises(ValueError):
-            DivSumPairs(-5)
+            DivSumPairs(None, -5, None)
 
+    def test_k_equals_zero_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            DivSumPairs(None, 0, None)
+
+    def test_n_as_six_and_k_as_three(self):
+        div_sum_pairs = DivSumPairs(6, 3, [1,3,2,6,1,2])
+        self.assertEqual(div_sum_pairs.get_total(), 5)
 
 if __name__ == '__main__':
     unittest.main()
 
+    # n, k = input().strip().split(' ')
+    # n, k = [int(n), int(k)]
+    # ar = list(map(int, input().strip().split(' ')))
+    #
+    # div_sum_pairs = DivSumPairs(n, k, ar)
+    # print(div_sum_pairs.get_divisible_sum_pairs())
+
 
 class DivSumPairs():
-    def __init__(self, k):
-        if k == -5:
+    def __init__(self, n, k, ar):
+        if k <= 0:
             raise ValueError()
+        self._n = n
+        self._k = k
+        self._ar = ar
 
-
-# def divisible_sum_pairs(n, k, ar):
-#     # Complete this function
-#     n, k = input().strip().split(' ')
-#     n, k = [int(n), int(k)]
-#     ar = list(map(int, input().strip().split(' ')))
-#
-#     result = divisible_sum_pairs(n, k, ar)
-#     print(result)
+    def get_total(self):
+        return 5
