16d6c74a6324d3a29809dd0273d406dbb2dbfabd
Pedro Soares <placerda@Pedros-MacBook-Pro.local>
2018-04-16 16:20:40 -0300

    refactoring is_leap(year) function

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,7 +4,9 @@ import sys
 
 
 def is_leap(year):
-    return (year % 4 == 0 and year % 100 != 0) or (year % 4 == 0 and year % 100 == 0 and year % 400 == 0)
+    if year % 100 == 0 and not year % 400 == 0:
+        return False
+    return year % 4 == 0
     # return is_typical_leap_year(year) and not is_atypical_common_year(year)
 
 # def is_atypical_common_year(year):
