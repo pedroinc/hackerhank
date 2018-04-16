a68b4fe8c9c04a8acc961532f19eac6d3b06a3a7
Pedro Soares <placerda@Pedros-MacBook-Pro.local>
2018-04-16 16:28:26 -0300

    more refactoring: test_leapyear.py

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,26 +4,22 @@ import sys
 
 
 def is_leap(year):
-    if year % 100 == 0 and not year % 400 == 0:
-        return False
+    return is_typical_leap_year(year) and not is_atypical_common_year(year)
+
+def is_atypical_common_year(year):
+    return year % 100 == 0 and not year % 400 == 0
+
+def is_typical_leap_year(year):
     return year % 4 == 0
-    # return is_typical_leap_year(year) and not is_atypical_common_year(year)
 
-# def is_atypical_common_year(year):
-#     return year % 100 == 0 and not year % 400 == 0
-#
-# def is_typical_leap_year(year):
-#     return year % 4 == 0
-
-
-def test_1996_is_leap_year():
+def test_normal_leap_year():
     assert is_leap(1996)
 
-def test_2001_is_not_leap_year():
+def test_normal_common_year():
     assert not is_leap(2001)
 
-def test_2000_is_leap_year():
-    assert is_leap(2000)
-
-def test_1900_is_not_leap_year():
+def test_atypical_common_year():
     assert not is_leap(1900)
+
+def test_atypical_leap_year():
+    assert is_leap(2000)
