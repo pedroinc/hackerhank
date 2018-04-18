83f95ba56fd2dacc22905aa4a8492348284bf01d
Pedro Soares <pedro.soares.lacerda@tvglobo.com.br>
2018-04-18 20:31:07 -0300

    imagecollector with new feature: downloading lots of images

--- imagecollector.py
+++ imagecollector.py
@@ -5,28 +5,48 @@ from urllib import request
 class ImageCollector:
 
     example_img = 'https://s03.video.glbimg.com/x720/6671490.jpg'
+    example_img_folder = 'https://s03.video.glbimg.com/x720/'
     IMAGE_FOLDER = "/Users/placerda/Desktop/img_files/"
 
-    def __init__(self, img_url):
+    def __init__(self, img_url=None, img_folder=None):
+        self._img_url_folder = img_folder
         self._img_url = img_url
 
     def is_valid_file(self):
         return self._img_url.endswith('.jpg')
 
+    @staticmethod
+    def get_file_number():
+        return len(os.listdir(ImageCollector.IMAGE_FOLDER))
+
     def download_file(self):
         try:
-            files = os.listdir(ImageCollector.IMAGE_FOLDER)
-            filename = 1 if len(files) == 0 else len(files)
+            file_number = self.get_file_number()
+            filename = 1 if file_number == 0 else file_number
             f = open(ImageCollector.IMAGE_FOLDER + "{0}.jpg".format(filename), 'wb')
             file_stream = request.urlopen(self._img_url).read()
             f.write(file_stream)
         except Exception as e:
             print(str(e))
 
+    def download_file(self, number):
+        try:
+            file_number = self.get_file_number()
+            filename = 1 if file_number == 0 else file_number
+            f = open(ImageCollector.IMAGE_FOLDER + "{0}.jpg".format(filename), 'wb')
+            print("{0}{1}.jpg".format(self._img_url_folder, number))
+            file_stream = request.urlopen("{0}{1}.jpg".format(self._img_url_folder, number)).read()
+            f.write(file_stream)
+        except Exception as e:
+            print("erro: {0}".format(e))
+
 
 if __name__ == '__main__':
 
-    image_collector = ImageCollector(ImageCollector.example_img)
+    image_collector = ImageCollector(img_folder=ImageCollector.example_img_folder)
 
-    for item in range(0, 10):
-        image_collector.download_file()
+    start_pos = 6671400
+    end_pos = 6671500
+
+    for i in range(start_pos, end_pos):
+        image_collector.download_file(i)
